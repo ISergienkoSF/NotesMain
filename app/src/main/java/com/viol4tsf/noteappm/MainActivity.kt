7b9e1d3c545592f199ffeb371f2163ab64aea38d
ISergienkoSF <sergienko.ivan2020@mail.ru>
2023-01-04 16:17:08 +0300

    DB and recycler view

--- app/src/main/java/com/viol4tsf/noteappm/MainActivity.kt
+++ app/src/main/java/com/viol4tsf/noteappm/MainActivity.kt
@@ -2,11 +2,18 @@ package com.viol4tsf.noteappm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.viol4tsf.noteappm.databinding.ActivityMainBinding
+import com.viol4tsf.noteappm.db.NoteDatabase
+import com.viol4tsf.noteappm.repository.NoteRepository
+import com.viol4tsf.noteappm.viewmodel.NoteViewModel
+import com.viol4tsf.noteappm.viewmodel.NoteViewModelProviderFactory
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding;
+    private lateinit var binding: ActivityMainBinding
+
+    lateinit var noteViewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +21,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+        setUpViewModel()
+    }
+
+    private fun setUpViewModel(){
+
+        val noteRepository = NoteRepository(
+            NoteDatabase(this)
+        )
+
+        val viewModelProviderFactory = NoteViewModelProviderFactory(
+            application,
+            noteRepository
+        )
+
+        noteViewModel = ViewModelProvider(
+            this,
+            viewModelProviderFactory
+        ).get(NoteViewModel::class.java)
     }
 }
---
+++ app/src/main/java/com/viol4tsf/noteappm/adapter/NoteAdapter.kt
@@ -0,0 +1,52 @@
+package com.viol4tsf.noteappm.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.viol4tsf.noteappm.databinding.NoteItemBinding
+import com.viol4tsf.noteappm.model.Note
+
+class NoteAdapter: RecyclerView.Adapter<NoteAdapter.NoteViewHolder>() {
+
+    private var binding: NoteItemBinding? = null
+
+    class NoteViewHolder(itemBinding: NoteItemBinding): RecyclerView.ViewHolder(itemBinding.root)
+
+    private val differCallback = object : DiffUtil.ItemCallback<Note>(){
+        override fun areItemsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Note, newItem: Note): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        binding = NoteItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+
+        return NoteViewHolder(binding!!)
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val currentNote = differ.currentList[position]
+
+        holder.itemView.apply {
+
+            binding?.noteTitleTextView?.text = currentNote.noteTitle
+            binding?.noteBodyTextView?.text = currentNote.noteBody
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+}
